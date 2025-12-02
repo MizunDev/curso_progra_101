281122b3ad9bbda5b8360f5bc10f58401c498185
MizunDev <jmperillap@gmail.com>
2025-12-01 19:05:03 -0500

    app.ts actualizado

--- app.ts
+++ app.ts
@@ -2,4 +2,10 @@
 let firstName = "Manuel";
 let lastName = "Perilla";
 
-console.log("Nombre Completo: " + firstName + " " + lastName);
+//console.log(firstName, lastName);
+let fullName = `${firstName} ${lastName}`;
+console.log(fullName);
+
+let number1 = 10;
+let number2 = 20;
+console.log(`El resultado de la resta es: ${number1 - number2}`);
