228c5051fe7b4c1ef9f26bb2efb07b6d90c98d87
MizunDev <jmperillap@gmail.com>
2025-12-03 17:04:08 -0500

    app.ts estructuras de control actualizado 2

--- app.ts
+++ app.ts
@@ -3,9 +3,11 @@ console.log('Inicio de Programa')
 
 let isTired = true;
 
-if (isTired === true) {
+if (isTired) {
     console.log('Estoy cansado, necesito descansar');
-} else {
+}
+
+if (!isTired) {
     console.log('No estoy cansado, puedo seguir trabajando');
 }
 
