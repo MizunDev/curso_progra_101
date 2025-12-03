92285e80389fec556f95a95c740682cbf2014390
MizunDev <jmperillap@gmail.com>
2025-12-03 17:15:49 -0500

    creacion if-else en sections y app.ts actualizado

--- app.ts
+++ app.ts
@@ -1,16 +1,1 @@
-console.log('Inicio de Programa')
-
-
-let isTired = true;
-
-if (isTired) {
-    console.log('Estoy cansado, necesito descansar');
-}
-
-if (!isTired) {
-    console.log('No estoy cansado, puedo seguir trabajando');
-}
-
-
-
-console.log('Fin de Programa')
+console.log('Hola Mundo')
