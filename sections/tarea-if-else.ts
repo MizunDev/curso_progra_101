cb6d4f943dd977f355511dbfacbd184f607ceec5
MizunDev <jmperillap@gmail.com>
2025-12-03 17:23:19 -0500

    creacion tarea-if-else en sections

---
+++ sections/tarea-if-else.ts
@@ -0,0 +1,15 @@
+console.log('Hola Mundo')
+
+export let notaAlumno: number = 59;
+
+if (notaAlumno >= 90) {
+    console.log('A');
+} else if (notaAlumno >= 80) {
+    console.log('B');
+}   else if (notaAlumno >= 70) {
+    console.log('C');
+}       else if (notaAlumno >= 60) {
+    console.log('D');
+} else {
+    console.log('F');
+}
